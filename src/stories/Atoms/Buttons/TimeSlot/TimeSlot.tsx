a0984671db9280bab6537cfc89741587efa5f4bb
Maxime Maufront <maximemaufront@mbp-de-maxime.home>
2025-09-05 14:23:45 +0200

    refacto border timeslot

--- src/stories/Atoms/Buttons/TimeSlot/TimeSlot.tsx
+++ src/stories/Atoms/Buttons/TimeSlot/TimeSlot.tsx
@@ -18,7 +18,7 @@ export default function TimeSlot({
       disabled={disabled}
       className={`text-greyscale-700 h-[52px] w-full px-4 py-2 font-light border-b border-greyscale-400 enabled:hover:bg-greyscale-200 enabled:hover:text-greyscale-900 enabled:hover:font-normal  ${
         isSelected
-          ? "bg-primary-800 border-primary-800 text-white font-semibold"
+          ? "bg-primary-800 border-primary-800 text-white font-semibold hover:border-b-primary-800 border-b-greyscale-200"
           : ""
       } disabled:bg-greyscale-100 disabled:text-greyscale-500`}
     >
